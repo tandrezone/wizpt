8aa176660d7feab3325653ad2560ea69e0d07e92
Tiago André <tiago.andre@wiz.pt>
2017-11-07 15:21:21 +0000

    change views

--- src/Traits/FrontendPages.php
+++ src/Traits/FrontendPages.php
@@ -28,10 +28,8 @@ trait FrontendPages
         if($needToChangeLang) {
             return $needToChangeLang;
         }
-
     	$page = $this->get($slug);
-
-        return View::make('pages.frontend')
+        return View::make('admin.pages.frontend')
             ->with('page',$page[0])
             ->with('pageCtrl',$this);
     }
